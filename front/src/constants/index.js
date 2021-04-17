fd15196af35fc99a4350475a38140172666a9588
yamori-masato <yamorimasato@gmail.com>
2021-04-18 02:54:24 +0900

    fix

--- front/src/constants/index.js
+++ front/src/constants/index.js
@@ -1,5 +1,5 @@
-export const API_ROOT = `http://localhost${process.env.PORT || 3001}`;
-export const API_WS_ROOT = `ws://localhost${process.env.PORT || 3001}/cable`;
+export const API_ROOT = `https://localhost${process.env.PORT || 3001}`;
+export const API_WS_ROOT = `wss://localhost${process.env.PORT || 3001}/cable`;
 export const HEADERS = {
     'Content-Type': 'application/json',
     Accept: 'application/json',
