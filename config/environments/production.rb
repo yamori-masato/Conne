60cf2bf835bd62ef598a2981ce8a0f69c6e88c2a
yamori-masato <yamorimasato@gmail.com>
2021-04-18 17:19:34 +0900

    test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,9 +33,11 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Mount Action Cable outside main process or domain
+  ActionCable.server.config.disable_request_forgery_protection = true
   # config.action_cable.mount_path = nil
-  config.action_cable.url = 'wss://example.com/cable'
-  config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  # config.action_cable.url = 'wss://example.com/cable'
+  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.allowed_request_origins = [/http:\/\/*/, /https:\/\/*/]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
