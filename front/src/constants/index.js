e35a5f65a8f8f644b6c6ba8dbf7575a3d6c57b54
yamori-masato <yamorimasato@gmail.com>
2021-04-18 17:51:38 +0900

    test

--- front/src/constants/index.js
+++ front/src/constants/index.js
@@ -1,5 +1,5 @@
 export const API_ROOT = `http://localhost:${process.env.PORT || 3001}`;
-export const API_WS_ROOT = `wss://localhost:${process.env.PORT || 3001}/cable`;
+export const API_WS_ROOT = `wss://link-game.herokuapp.com/cable`;
 export const HEADERS = {
     'Content-Type': 'application/json',
     Accept: 'application/json',
