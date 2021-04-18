ba8792c984b8c6b669535fc0d732854f79d922bc
yamori-masato <yamorimasato@gmail.com>
2021-04-18 17:47:35 +0900

    test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,9 +35,8 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   ActionCable.server.config.disable_request_forgery_protection = true
   # config.action_cable.mount_path = nil
-  config.action_cable.url = 'wss://localhogst:3001/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
-  config.action_cable.allowed_request_origins = [/http:\/\/*/, /https:\/\/*/]
+  config.action_cable.url = 'wss://link-game.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = ['https://link-game.herokuapp.com', /http:\/\/link-game.herokuapp.com.*/]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
